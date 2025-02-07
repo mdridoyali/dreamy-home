9ad9dc34aa23cf1607ff58eb323d76d27e2756e1
mdridoyali <mdridoyislamr@gmail.com>
2025-02-07 11:02:51 +0600

    navlink component created for advance design

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,4 +1,4 @@
-// "use client";
+"use client";
 
 import { Sheet, SheetClose, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
@@ -7,9 +7,14 @@ import Link from "next/link";
 import Image from "next/image";
 import logo from '@/app/assets/logo.png'
 import { FaHome, FaServicestack, FaArrowRight } from "react-icons/fa";
-import { MdRoundaboutRight, MdAddCall } from "react-icons/md";
+import { MdRoundaboutRight, MdAddCall, MdLogin } from "react-icons/md";
+import { usePathname } from "next/navigation";
+import NavLink from "../NavLink";
 
 export default function Navbar() {
+
+    const pathname = usePathname();
+
     return (
         <div className="border-b shadow-md">
             <nav className="max-w-[1480px] flex justify-between items-center mx-auto py-2 px-5 ">
@@ -29,44 +34,31 @@ export default function Navbar() {
                     <SheetContent side="left">
                         <div className="flex flex-col gap-5 mt-10">
                             <SheetClose asChild>
-                                <Link href="/" className="text-lg font-medium flex items-center justify-between hover:bg-gray-200 rounded-sm hover:py-2 transition-all "><span className="flex items-center gap-4"><FaHome />Home</span><FaArrowRight /></Link>
+                                <Link href="/" className={`text-lg font-medium flex items-center justify-between hover:bg-gray-200 rounded-sm hover:py-2 transition-all ${pathname === '/' ? "text-blue-600 font-semibold" : "text-gray-800"} `}><span className="flex items-center gap-4"><FaHome />Home</span><FaArrowRight /></Link>
                             </SheetClose>
                             <SheetClose asChild>
-                                <Link href="/service" className="text-lg font-medium flex items-center  justify-between  hover:bg-gray-200 rounded-sm hover:py-2 transition-all"><span className="flex items-center gap-4"><FaServicestack />Service</span><FaArrowRight /></Link>
+                                <Link href="/service" className={`text-lg font-medium flex items-center justify-between hover:bg-gray-200 rounded-sm hover:py-2 transition-all ${pathname === '/service' ? "text-blue-600 font-semibold" : "text-gray-800"} `}><span className="flex items-center gap-4"><FaServicestack />Service</span><FaArrowRight /></Link>
                             </SheetClose>
                             <SheetClose asChild>
-                                <Link href="/about" className="text-lg font-medium flex items-center  justify-between  hover:bg-gray-200 rounded-sm hover:py-2 transition-all"><span className="flex items-center gap-4"><MdRoundaboutRight />About Us</span><FaArrowRight /></Link>
+                                <Link href="/about" className={`text-lg font-medium flex items-center justify-between hover:bg-gray-200 rounded-sm hover:py-2 transition-all ${pathname === '/about' ? "text-blue-600 font-semibold" : "text-gray-800"} `}><span className="flex items-center gap-4"><MdRoundaboutRight />About Us</span><FaArrowRight /></Link>
                             </SheetClose>
                             <SheetClose asChild>
-                                <Link href="/contact" className="text-lg font-medium flex items-center  justify-between  hover:bg-gray-200 rounded-sm hover:py-2 transition-all"><span className="flex items-center gap-4"><MdAddCall />Contact Us</span><FaArrowRight /></Link>
+                                <Link href="/contact" className={`text-lg font-medium flex items-center justify-between hover:bg-gray-200 rounded-sm hover:py-2 transition-all ${pathname === '/contact' ? "text-blue-600 font-semibold" : "text-gray-800"} `}><span className="flex items-center gap-4"><MdAddCall />Contact Us</span><FaArrowRight /></Link>
+                            </SheetClose>
+                            <SheetClose asChild>
+                                <Link href="/login" className={`text-lg font-medium flex items-center justify-between hover:bg-gray-200 rounded-sm hover:py-2 transition-all ${pathname === '/login' ? "text-blue-600 font-semibold" : "text-gray-800"} `}><span className="flex items-center gap-4"><MdLogin />Login</span><FaArrowRight /></Link>
                             </SheetClose>
                         </div>
                     </SheetContent>
                 </Sheet>
 
                 {/* Desktop Menu */}
-                {/* Desktop Menu */}
                 <div className="hidden md:flex gap-6">
-                    <Link href="/" className="text-lg font-medium relative group">
-                        Home
-                        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
-                    </Link>
-                    <Link href="/service" className="text-lg font-medium relative group">
-                        Service
-                        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
-                    </Link>
-                    <Link href="/about" className="text-lg font-medium relative group">
-                        About Us
-                        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
-                    </Link>
-                    <Link href="/contact" className="text-lg font-medium relative group">
-                        Contact Us
-                        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
-                    </Link>
-                    <Link href="/login" className="text-lg font-medium relative group">
-                        Login
-                        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
-                    </Link>
+                    <NavLink href="/" currentPath={pathname}>Home</NavLink>
+                    <NavLink href="/service" currentPath={pathname}>Service</NavLink>
+                    <NavLink href="/about" currentPath={pathname}>About Us</NavLink>
+                    <NavLink href="/contact" currentPath={pathname}>Contact Us</NavLink>
+                    <NavLink href="/login" currentPath={pathname}>Login</NavLink>
                 </div>
 
             </nav>
