efd52d9744fd23c7e2536574a1ea67ea77e30bfc
mdridoyali <mdridoyislamr@gmail.com>
2025-02-07 23:05:38 +0600

    what we offer section done

--- src/components/home/WhatWeOffer.jsx
+++ src/components/home/WhatWeOffer.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import SectionTitle from '../SectionTitle';
 import Image from 'next/image';
 import home from '@/app/assets/whatWeOffer/home.png'
+import office from '@/app/assets/whatWeOffer/office.png'
+import furniture from '@/app/assets/whatWeOffer/furniture.png'
+import color from '@/app/assets/whatWeOffer/color.png'
 
 const WhatWeOffer = () => {
     return (
@@ -14,17 +17,17 @@ const WhatWeOffer = () => {
                     <p className='text-sm text-[#54595f]'>Modern, classic, and luxurious designs</p>
                 </div>
                 <div className='flex flex-col justify-center items-center p-6 md:p-10 space-y-5 bg-[#f5f5f5] hover:bg-[#e8e8e8] transition-all shadow-md rounded-md cursor-pointer'>
-                    <Image width={70} src={home} />
+                    <Image width={70} src={office} />
                     <h2 className='text-2xl font-semibold text-[#54595f]'>Office & Commercial Spaces</h2>
                     <p className='text-sm text-[#54595f]'>Functional, creative, and inspiring workplaces</p>
                 </div>
                 <div className='flex flex-col justify-center items-center p-6 md:p-10 space-y-5 bg-[#f5f5f5] hover:bg-[#e8e8e8] transition-all shadow-md rounded-md cursor-pointer'>
-                    <Image width={70} src={home} />
+                    <Image width={70} src={furniture} />
                     <h2 className='text-2xl font-semibold text-[#54595f]'>Furniture & Decor Consultation</h2>
                     <p className='text-sm text-[#54595f]'>Custom pieces that enhance your space</p>
                 </div>
                 <div className='flex flex-col justify-center items-center p-6 md:p-10 space-y-5 bg-[#f5f5f5] hover:bg-[#e8e8e8] transition-all shadow-md rounded-md cursor-pointer'>
-                    <Image width={70} src={home} />
+                    <Image width={70} src={color} />
                     <h2 className='text-2xl font-semibold text-[#54595f]'>Color & Theme Selection</h2>
                     <p className='text-sm text-[#54595f]'>Perfect palettes to match your style</p>
                 </div>
