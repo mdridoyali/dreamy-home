e8205fac3ceaba04bc7147370e95541db4b9eace
mdridoyali <mdridoyislamr@gmail.com>
2025-02-07 15:35:03 +0600

    search field added on the banner

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -6,6 +6,9 @@ import img2 from '@/app/assets/banner/2.jpg';
 import img3 from '@/app/assets/banner/3.jpg';
 import img4 from '@/app/assets/banner/4.jpg';
 import { useEffect, useState } from 'react';
+import { Button, } from '../ui/button';
+import { Input } from '../ui/input';
+import { FaSearch } from "react-icons/fa";
 
 const Banner = () => {
     const images = [img1, img2, img3, img4];
@@ -28,7 +31,7 @@ const Banner = () => {
         <div className="relative">
             <div className={`relative w-full h-screen transition-opacity duration-1000 ${fade ? 'opacity-100' : 'opacity-0'}`}>
                 <Image
-                    className="w-full h-full object-cover"
+                    className="w-full h-full"
                     src={images[currentImage]}
                     alt="Interior Design Banner"
                     priority
@@ -36,13 +39,17 @@ const Banner = () => {
             </div>
 
 
-            <div className="absolute top-[30%] left-1/2 transform -translate-x-1/2 text-center text-white bg-black bg-opacity-50 p-2">
+            <div className="absolute top-[20%] left-1/2 transform -translate-x-1/2 text-center text-white bg-black bg-opacity-50 p-2 rounded-md">
                 <h2 className="text-4xl md:text-6xl font-bold text-blue-500 mb-4">
                     Design Your Dream Space
                 </h2>
                 <p className="text-lg md:text-xl font-semibold max-w-lg mx-auto">
                     Transforming spaces with design that reflects your style and enhances your comfort.
                 </p>
+                <div className="flex mx-auto my-4 w-full max-w-sm items-center gap-1">
+                    <Input type="email" placeholder="Search" />
+                    <Button type="submit"><FaSearch/></Button>
+                </div>
             </div>
         </div>
     );
