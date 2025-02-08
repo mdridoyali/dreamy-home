2d7423965d666fec6eb8d7c686494ca054c7b83f
mdridoyali <mdridoyislamr@gmail.com>
2025-02-08 08:13:45 +0600

    our servise aria done

--- src/components/home/OurServiceAria.jsx
+++ src/components/home/OurServiceAria.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SectionTitle from '../SectionTitle';
 import img from '@/app/assets/serviceAria1.jpg';
 import Image from 'next/image';
+import { FaArrowRight } from "react-icons/fa";
 
 const OurServiceAria = () => {
     return (
@@ -16,15 +17,27 @@ const OurServiceAria = () => {
             </div>
 
             {/* Cards container */}
-            <div className='grid grid-cols-2 md:grid-cols-4 gap-5 w-11/12 mx-auto 
+            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5 w-11/12 mx-auto 
                 absolute left-1/2 -translate-x-1/2 -translate-y-1/2'>
                 {/* Each card */}
-                <div className='flex flex-col justify-center glass shadow-slate-400 items-center p-6 md:p-10 space-y-5 bg-[#ffffff] hover:bg-[#e8e8e8] transition-all shadow-md rounded-md cursor-pointer'>
-                    <h2 className='text-2xl font-semibold text-[#54595f]'>Dhaka</h2>
-                    <p className='text-sm text-[#54595f]'>Perfect palettes to match your style</p>
+                <div className='flex flex-col justify-center  shadow-slate-400 items-center p-6 md:p-10 space-y-5 bg-[#ffffff] hover:bg-[#e8e8e8] transition-all shadow-md rounded-md cursor-pointer'>
+                    <h2 className='text-3xl font-semibold bg-slate-100 rounded-xl p-5 flex items-center gap-5 text-blue-600'>Dhaka<FaArrowRight/></h2>
                 </div>
+                <div className='flex flex-col justify-center  shadow-slate-400 items-center p-6 md:p-10 space-y-5 bg-[#ffffff] hover:bg-[#e8e8e8] transition-all shadow-md rounded-md cursor-pointer'>
+                    <h2 className='text-3xl font-semibold bg-slate-100 rounded-xl p-5 flex items-center gap-5 text-blue-600'>Khulna<FaArrowRight/></h2>
+                </div>
+                <div className='flex flex-col justify-center  shadow-slate-400 items-center p-6 md:p-10 space-y-5 bg-[#ffffff] hover:bg-[#e8e8e8] transition-all shadow-md rounded-md cursor-pointer'>
+                    <h2 className='text-3xl font-semibold bg-slate-100 rounded-xl p-5 flex items-center gap-5 text-blue-600'>Rajshahi<FaArrowRight/></h2>
+                </div>
+                <div className='flex flex-col justify-center  shadow-slate-400 items-center p-6 md:p-10 space-y-5 bg-[#ffffff] hover:bg-[#e8e8e8] transition-all shadow-md rounded-md cursor-pointer'>
+                    <h2 className='text-3xl font-semibold bg-slate-100 rounded-xl p-5 flex items-center gap-5 text-blue-600'>Chattogram<FaArrowRight/></h2>
+                </div>
+                <div className='flex flex-col justify-center  shadow-slate-400 items-center p-6 md:p-10 space-y-5 bg-[#ffffff] hover:bg-[#e8e8e8] transition-all shadow-md rounded-md cursor-pointer'>
+                    <h2 className='text-3xl font-semibold bg-slate-100 rounded-xl p-5 flex items-center gap-5 text-blue-600'>Sylhet<FaArrowRight/></h2>
+                </div>
+          
 
-               
+                
             </div>
         </div>
     );
