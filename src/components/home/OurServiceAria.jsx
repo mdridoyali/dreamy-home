934ccbdd57260d15309f3027713b6967422ab1e9
mdridoyali <mdridoyislamr@gmail.com>
2025-02-08 07:43:42 +0600

    our service aria section created

--- src/components/home/OurServiceAria.jsx
+++ src/components/home/OurServiceAria.jsx
@@ -7,22 +7,29 @@ const OurServiceAria = () => {
     return (
         <div className='my-32 mb-56 relative'>
             <Image className='h-[70vh] object-cover' src={img} alt="Service Area" />
-            <div className='absolute top-[120px] left-1/2  -translate-x-1/2 -translate-y-1/2 text-center'>
+            <div className='absolute top-[120px] left-1/2 -translate-x-1/2 -translate-y-1/2 text-center'>
                 <SectionTitle
                     className='text-white'
                     title={'Our Service Area'}
                     titleDescription={'Discover our expert interior design solutions tailored to transform your home or workplace into a masterpiece.'}
                 />
             </div>
-            <div className='flex flex-col justify-center items-center p-6 md:p-10 space-y-5 bg-[#f5f5f5] hover:bg-[#e8e8e8]
-                 transition-all shadow-md rounded-md cursor-pointer absolute -bottom-20 left-1/2 transform -translate-x-1/2'>
-                <h2 className='text-2xl font-semibold text-[#54595f]'>Color & Theme Selection</h2>
-                <p className='text-sm text-[#54595f]'>Perfect palettes to match your style</p>
+
+            {/* Cards container */}
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-5 w-11/12 mx-auto 
+                absolute left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                {/* Each card */}
+                <div className='flex flex-col justify-center glass shadow-slate-400 items-center p-6 md:p-10 space-y-5 bg-[#ffffff] hover:bg-[#e8e8e8] transition-all shadow-md rounded-md cursor-pointer'>
+                    <h2 className='text-2xl font-semibold text-[#54595f]'>Dhaka</h2>
+                    <p className='text-sm text-[#54595f]'>Perfect palettes to match your style</p>
+                </div>
+
+               
             </div>
-         
-         
         </div>
     );
 };
 
 export default OurServiceAria;
+
+{/* <div class="shadow-lg shadow-blue-400 md:w-96 glass h-[120px] relative mt-7 rounded-lg aos-init aos-animate" data-aos="flip-right"></div> */}
