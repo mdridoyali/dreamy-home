ad2d430f0c747e25d042d6d11f0278a86ab87a2f
mdridoyali <mdridoyislamr@gmail.com>
2025-02-09 21:48:24 +0600

    fix bugs

--- src/components/home/TrustedBrands.jsx
+++ src/components/home/TrustedBrands.jsx
@@ -10,22 +10,25 @@ import nasir from "@/app/assets/companyLogo/nasir.jpg";
 import vision from "@/app/assets/companyLogo/vision.jpg";
 import square from "@/app/assets/companyLogo/square.jpg";
 import rfl from "@/app/assets/companyLogo/rfl.jpg";
+import parallax from "@/app/assets/parallax.jpg";
 
 const TrustedBrands = () => {
     return (
-        <div className="my-20 ">
-                <div className=" w-11/12 mx-auto py-5 ">
-                    <SectionTitle title="Trusted by Leading Brands" titleDescription="We have worked with 100+ trusted companies." />
-                    <div className="w-8/12 mx-auto grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5  justify-center items-center text-center ">
-                        <Image className="" width={100} src={walton} alt="Walton" />
-                        <Image width={100} src={rfl} alt="RFL" />
-                        <Image width={100} src={brb} alt="BRB" />
-                        <Image width={100} src={nasir} alt="Nasir" />
-                        <Image width={100} src={vision} alt="Vision" />
-                        <Image width={100} src={square} alt="Square" />
-                    </div>
+        <div className="my-20 relative">
+            <Image className="w-full h-[100vh] md:h-[70vh] object-cover" src={parallax} alt="Parallax" />
+            <div className="w-11/12 mx-auto py-5 absolute top-10 left-0 right-0">
+                <SectionTitle pColor={'text-white'} title="Trusted by Leading Brands" titleDescription="We have worked with 100+ trusted companies." />
+                <div className="w-1/2 mx-auto grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5 justify-center items-center text-center">
+                    <Image className="mx-auto" width={100} src={walton} alt="Walton" />
+                    <Image className="mx-auto" width={100} src={rfl} alt="RFL" />
+                    <Image className="mx-auto" width={100} src={brb} alt="BRB" />
+                    <Image className="mx-auto" width={100} src={nasir} alt="Nasir" />
+                    <Image className="mx-auto" width={100} src={vision} alt="Vision" />
+                    <Image className="mx-auto" width={100} src={square} alt="Square" />
                 </div>
+            </div>
         </div>
+
 
     );
 };
